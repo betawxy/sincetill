cc6b45cb9b2b9d2362edc7b2535552c4294e31ac
betawxy <betawxy@gmail.com>
2021-04-04 00:23:32 -0700

    polish nav

--- web/components/PageWrapper.tsx
+++ web/components/PageWrapper.tsx
@@ -9,29 +9,37 @@ export default function PageWrapper({
   children: JSX.Element | JSX.Element[];
 }) {
   return (
-    <div className="container mx-auto">
-      <nav className="flex items-center mb-3 bg-indigo-50 p-6 rounded-b">
-        <div className="flex-grow">
-          <div className="inline-block">
-            <Link href="/">
-              <h1 className="text-3xl cursor-pointer">since till</h1>
+    <div>
+      <nav className="mb-3 bg-indigo-500 p-4">
+        <div className="container mx-auto flex items-center">
+          <NextNprogress
+            color="#a8dadc"
+            startPosition={0}
+            stopDelayMs={50}
+            height={2}
+          />
+          <div className="flex-grow">
+            <div className="inline-block">
+              <Link href="/">
+                <h1 className="text-white text-3xl cursor-pointer">
+                  since till
+                </h1>
+              </Link>
+            </div>
+          </div>
+          <div className="flex-none">
+            <Link href="/item/add">
+              <div className="beta-link text-indigo-50 hover:text-white">
+                Add Item
+              </div>
             </Link>
           </div>
         </div>
-        <div className="flex-none">
-          <Link href="/item/add">
-            <div className="beta-link">Add Item</div>
-          </Link>
-        </div>
       </nav>
-      <main>{children}</main>
-      <footer></footer>
-      <NextNprogress
-        color="#00afb9"
-        startPosition={0}
-        stopDelayMs={50}
-        height={2}
-      />
+      <div className="container mx-auto">
+        <main>{children}</main>
+        <footer></footer>
+      </div>
     </div>
   );
 }
