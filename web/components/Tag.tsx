7e62770430dad34ef2e57e50332060405d58b799
betawxy <betawxy@gmail.com>
2021-04-05 01:31:26 -0700

    fix PurgeCSS issue

--- web/components/Tag.tsx
+++ web/components/Tag.tsx
@@ -6,11 +6,43 @@ export default function Tag({
   className,
 }: {
   text: string;
-  color: string;
+  color: "green" | "pink" | "yellow";
   className: string;
 }) {
-  const cx =
-    `inline-block border rounded-sm text-center text-xs text-${color}-500 border-${color}-200 bg-${color}-50 ` +
-    className;
-  return <span className={cx}>{text}</span>;
+  // duplicate code to avoid PurgeCSS purging concatenated className
+  switch (color) {
+    case "green":
+      return (
+        <span
+          className={
+            "inline-block border rounded-sm text-center text-xs text-green-500 border-green-200 bg-green-50 " +
+            className
+          }
+        >
+          {text}
+        </span>
+      );
+    case "pink":
+      return (
+        <span
+          className={
+            "inline-block border rounded-sm text-center text-xs text-pink-500 border-pink-200 bg-pink-50 " +
+            className
+          }
+        >
+          {text}
+        </span>
+      );
+    case "yellow":
+      return (
+        <span
+          className={
+            "inline-block border rounded-sm text-center text-xs text-yellow-500 border-yellow-200 bg-yellow-50 " +
+            className
+          }
+        >
+          {text}
+        </span>
+      );
+  }
 }
