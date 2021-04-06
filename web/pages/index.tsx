238da5fe5fcbebb5b8568f05da34372555693718
betawxy <betawxy@gmail.com>
2021-04-06 16:51:20 -0700

    basic landing page

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -3,11 +3,25 @@ import React from "react";
 
 export default function LandingPage() {
   return (
-    <div>
-      <div className="w-full bg-green-100 p-10">Landing Page</div>
-      <Link href="/items">
-        <span className="beta-link m-10">Go to web app</span>
-      </Link>
+    <div className="w-screen h-screen bg-gray-50">
+      <nav className="w-full h-16 bg-indigo-800">
+        <div className="container w-full h-16 mx-auto flex max-w-screen-lg">
+          <div className="flex self-center text-white text-3xl">since till</div>
+        </div>
+      </nav>
+      <main className="container mx-auto max-w-screen-lg">
+        <div className="flex my-6">
+          <div className="w-2/3 bg-yellow-100 p-10 rounded mr-3">
+            <h1 className="text-xl mb-3">Landing Page</h1>
+            <Link href="/items">
+              <span className="beta-link">Open web app</span>
+            </Link>
+          </div>
+          <div className="w-1/3 bg-yellow-100 p-10 rounded ml-3">
+            <div>Download Apps</div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
