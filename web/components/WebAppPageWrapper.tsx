a1413e3e829fefd8436831cea65533f3e1fb97e4
betawxy <betawxy@gmail.com>
2021-04-06 17:10:43 -0700

    basic footer

--- web/components/WebAppPageWrapper.tsx
+++ web/components/WebAppPageWrapper.tsx
@@ -15,10 +15,15 @@ export default function WebAppPageWrapper({
       <main>
         <div className="container mx-auto max-w-screen-md">{children}</div>
       </main>
-      <footer>
-        <Link href="/">
-          <div className="beta-link h-10 bg-blue-100"></div>
-        </Link>
+      <footer className="h-16 mt-20 bg-indigo-100 border-t-2 border-indigo-200 border-dashed">
+        <div className="flex container max-w-screen-md mx-auto h-16">
+          <div className="flex self-center">
+            <span className="text-indigo-600 text-sm">
+              &copy;{new Date().getFullYear() + " "}
+              <Link href="/">SinceTill.com</Link>
+            </span>
+          </div>
+        </div>
       </footer>
     </AuthRedirect>
   );
