79ab8072b5d20a8d54673e7dfc3e273eb92804f1
betawxy <betawxy@gmail.com>
2021-04-20 21:57:26 -0700

    load items

---
+++ android/app/src/main/java/com/sincetill/android/Item.java
@@ -0,0 +1,70 @@
+package com.sincetill.android;
+
+public class Item {
+    private String backgroundImage;
+    private long ctime;
+    private int formatType;
+    private String id;
+    private boolean isFullDayEvent;
+    private long mtime;
+    private String title;
+    private long ts;
+    private String uid;
+
+    public Item(String backgroundImage,
+                long ctime,
+                int formatType,
+                String id,
+                boolean isFullDayEvent,
+                long mtime,
+                String title,
+                long ts,
+                String uid
+    ) {
+        this.backgroundImage = backgroundImage;
+        this.ctime = ctime;
+        this.formatType = formatType;
+        this.id = id;
+        this.isFullDayEvent = isFullDayEvent;
+        this.mtime = mtime;
+        this.title = title;
+        this.ts = ts;
+        this.uid = uid;
+    }
+
+    public String getBackgroundImage() {
+        return backgroundImage;
+    }
+
+    public long getCtime() {
+        return ctime;
+    }
+
+    public int getFormatType() {
+        return formatType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isFullDayEvent() {
+        return isFullDayEvent;
+    }
+
+    public long getMtime() {
+        return mtime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getTs() {
+        return ts;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+}
--- android/app/src/main/java/com/sincetill/android/MainActivity.java
+++ android/app/src/main/java/com/sincetill/android/MainActivity.java
@@ -1,20 +1,32 @@
 package com.sincetill.android;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.sincetill.android.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -22,9 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding mBinding;
 
     private FirebaseAuth mFirebaseAuth;
-    private FirebaseDatabase mDatabase;
+    private FirebaseFirestore mFireStore;
 
     private GoogleSignInClient mSignInClient;
+
+    private CollectionReference userItemsRef;
+    private ArrayList<Item> userItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +56,75 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         mSignInClient = GoogleSignIn.getClient(this, gso);
 
+        mFireStore = FirebaseFirestore.getInstance();
+
+        final CollectionReference userItemsRef =
+                mFireStore.collection("users")
+                        .document(mFirebaseAuth.getCurrentUser().getUid())
+                        .collection("items");
+        loadAndSyncUserItems();
+    }
+
+    private void loadAndSyncUserItems() {
+        userItemsRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                        Item item = document.toObject(Item.class);
+                        userItems.add(item);
+                    }
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+
+        userItemsRef.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot snapshots,
+                                @Nullable FirebaseFirestoreException error) {
+                if (error != null) {
+                    Log.w(TAG, "Listen failed.", error);
+                    return;
+                }
+
+                for (DocumentChange dc : snapshots.getDocumentChanges()) {
+                    Item item = dc.getDocument().toObject(Item.class);
+                    int i = -1;
+                    switch (dc.getType()) {
+                        case ADDED:
+                            Log.d(TAG, "New item: " + dc.getDocument().getData());
+                            userItems.add(item);
+                            break;
+                        case MODIFIED:
+                            Log.d(TAG, "Modified item: " + dc.getDocument().getData());
+                            i = findItem(item.getId());
+                            if (i != -1) {
+                                userItems.set(i, item);
+                            }
+                            break;
+                        case REMOVED:
+                            Log.d(TAG, "Removed item: " + dc.getDocument().getData());
+                            i = findItem(item.getId());
+                            if (i != -1) {
+                                userItems.remove(i);
+                            }
+                            break;
+                    }
+                }
+            }
+
+            private int findItem(String id) {
+                for (int i = 0; i < userItems.size(); i++) {
+                    if (userItems.get(i).getId().equals(id)) {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        });
     }
 
     @Override
