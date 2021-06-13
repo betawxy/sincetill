11eebd1a9a0ce85c95a553869f706d369bd2faf1
betawxy <betawxy@gmail.com>
2021-06-13 01:58:23 -0700

    init FlutterFire

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:sincetill/screens/auth_screen.dart';
 import 'package:sincetill/screens/item_details_screen.dart';
 import 'package:sincetill/screens/item_list_screen.dart';
+import 'package:sincetill/screens/loading_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -13,8 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: ItemListScreen.route,
+      initialRoute: LoadingScreen.route,
       routes: {
+        LoadingScreen.route: (context) => LoadingScreen(),
         AuthScreen.route: (context) => AuthScreen(),
         ItemListScreen.route: (context) => ItemListScreen(),
         ItemDetailsScreen.route: (context) => ItemDetailsScreen(),
---
+++ flutter/lib/screens/loading_screen.dart
@@ -0,0 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:sincetill/screens/item_details_screen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  static const route = '/loading';
+
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  Widget wrapper(Widget child) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: child,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return wrapper(Text('something went wrong.'));
+        }
+
+        if (snapshot.connectionState != ConnectionState.done) {
+          return wrapper(Text('loading...'));
+        }
+
+        return ItemDetailsScreen();
+      },
+    );
+  }
+}
