5b0f28710eed61157e0cdfb71dad1e25c036d7c7
betawxy <betawxy@gmail.com>
2021-06-18 22:29:13 -0700

    new item state

--- flutter/lib/constants.dart
+++ flutter/lib/constants.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 var kAppTheme = ThemeData(
   primaryColor: const Color(0xFF3d5a80),
   accentColor: const Color(0xFFe56b6f),
+  inputDecorationTheme: InputDecorationTheme(
+    labelStyle: TextStyle(
+      color: const Color(0xFF3d5a80),
+    ),
+  ),
 );
 
 const kDefaultImageUrl =
--- flutter/lib/screens/add_item_screen.dart
+++ flutter/lib/screens/add_item_screen.dart
@@ -1,9 +1,13 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sincetill/models/item_model.dart';
+
+import 'auth_screen.dart';
 
 class AddItemScreen extends StatefulWidget {
   static const route = '/add';
@@ -15,7 +19,9 @@ class AddItemScreen extends StatefulWidget {
 }
 
 class _AddItemScreenState extends State<AddItemScreen> {
-  late String _title;
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  String? _title;
   bool _isFullDayEvent = false;
   var _formatType = EFormatType.DAYS;
   var _ts = DateTime.now();
@@ -25,6 +31,12 @@ class _AddItemScreenState extends State<AddItemScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final User? user = FirebaseAuth.instance.currentUser;
+
+    if (user == null) {
+      return AuthScreen();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Item'),
@@ -36,121 +48,166 @@ class _AddItemScreenState extends State<AddItemScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                FormBuilderTextField(
-                  name: 'title',
-                  decoration: InputDecoration(
-                    labelText: 'Title *',
-                  ),
-                  onChanged: (value) {
-                    if (value != null) {
-                      _title = value;
-                    }
+                FormBuilder(
+                  key: _formKey,
+                  autovalidateMode: AutovalidateMode.disabled,
+                  initialValue: {
+                    'title': '',
+                    'isFullDayEvent': false,
+                    'age': '13',
+                    'gender': 'Male'
                   },
-                  validator: FormBuilderValidators.compose([
-                    FormBuilderValidators.required(context),
-                  ]),
-                  keyboardType: TextInputType.text,
-                ),
-                FormBuilderSwitch(
-                  name: 'isFullDayEvent',
-                  title: Text('Full Day Event'),
-                  initialValue: false,
-                  onChanged: (value) {
-                    if (value != null) {
-                      setState(() {
-                        _isFullDayEvent = value;
-                      });
-                    }
-                  },
-                ),
-                FormBuilderDateTimePicker(
-                  name: 'date',
-                  inputType: InputType.date,
-                  decoration: InputDecoration(
-                    labelText: 'Date',
-                  ),
-                  initialValue: _ts,
-                  onChanged: (value) {
-                    if (value != null) _ts = value;
-                  },
-                ),
-                if (!_isFullDayEvent)
-                  FormBuilderDateTimePicker(
-                    name: 'time',
-                    inputType: InputType.time,
-                    decoration: InputDecoration(
-                      labelText: 'Time',
-                    ),
-                    initialValue: _ts,
-                    onChanged: (value) {
-                      if (value != null) _ts = value;
-                    },
-                  ),
-                FormBuilderDropdown(
-                  name: 'format_type',
-                  decoration: InputDecoration(
-                    labelText: 'Show As',
-                  ),
-                  hint: Text('Select Format'),
-                  validator: FormBuilderValidators.compose(
-                      [FormBuilderValidators.required(context)]),
-                  initialValue: _formatType,
-                  onChanged: (value) {
-                    if (value != null) {
-                      _formatType = value as EFormatType;
-                    }
-                  },
-                  items: EFormatType.values
-                      .map(
-                        (formatType) => DropdownMenuItem(
-                          value: formatType,
-                          child: Text(
-                            formatType.toString().split('.').last.capitalize(),
+                  skipDisabled: true,
+                  child: Column(
+                    children: [
+                      FormBuilderTextField(
+                        name: 'title',
+                        decoration: InputDecoration(
+                          labelText: 'Title *',
+                        ),
+                        onChanged: (value) {
+                          if (value != null) {
+                            _title = value;
+                          }
+                        },
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(context),
+                        ]),
+                        keyboardType: TextInputType.text,
+                      ),
+                      FormBuilderSwitch(
+                        name: 'isFullDayEvent',
+                        title: Text(
+                          'Full Day Event',
+                          style: TextStyle(
+                            color: Theme.of(context).primaryColor,
                           ),
                         ),
-                      )
-                      .toList(),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    Column(
-                      children: [
-                        IconButton(
-                          icon: Icon(Icons.photo_camera),
-                          onPressed: _pickImageFromCamera,
-                        ),
-                        IconButton(
-                          icon: Icon(Icons.photo),
-                          onPressed: _pickImageFromGallery,
-                        ),
-                      ],
-                    ),
-                    Flexible(
-                      child: Container(
-                        width: double.infinity,
-                        // TODO: change to background
-                        child: AspectRatio(
-                          aspectRatio: 1 / 1,
-                          child: this._imageFile == null
-                              ? Placeholder(
-                                  strokeWidth: 1,
-                                  fallbackHeight: 200,
-                                )
-                              : Image.file(this._imageFile!),
-                        ),
+                        onChanged: (value) {
+                          if (value != null) {
+                            setState(() {
+                              _isFullDayEvent = value;
+                            });
+                          }
+                        },
                       ),
-                    ),
-                  ],
+                      FormBuilderDateTimePicker(
+                        name: 'date',
+                        inputType: InputType.date,
+                        decoration: InputDecoration(
+                          labelText: 'Date',
+                        ),
+                        initialValue: _ts,
+                        onChanged: (value) {
+                          if (value != null) _ts = value;
+                        },
+                      ),
+                      if (!_isFullDayEvent)
+                        FormBuilderDateTimePicker(
+                          name: 'time',
+                          inputType: InputType.time,
+                          decoration: InputDecoration(
+                            labelText: 'Time',
+                          ),
+                          initialValue: _ts,
+                          onChanged: (value) {
+                            if (value != null) _ts = value;
+                          },
+                        ),
+                      FormBuilderDropdown(
+                        name: 'format_type',
+                        decoration: InputDecoration(
+                          labelText: 'Show As',
+                        ),
+                        hint: Text('Select Format'),
+                        validator: FormBuilderValidators.compose(
+                            [FormBuilderValidators.required(context)]),
+                        initialValue: _formatType,
+                        onChanged: (value) {
+                          if (value != null) {
+                            _formatType = value as EFormatType;
+                          }
+                        },
+                        items: EFormatType.values
+                            .map(
+                              (formatType) => DropdownMenuItem(
+                                value: formatType,
+                                child: Text(
+                                  formatType
+                                      .toString()
+                                      .split('.')
+                                      .last
+                                      .capitalize(),
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(
+                                icon: Icon(Icons.photo_camera),
+                                onPressed: _pickImageFromCamera,
+                              ),
+                              IconButton(
+                                icon: Icon(Icons.photo),
+                                onPressed: _pickImageFromGallery,
+                              ),
+                            ],
+                          ),
+                          Flexible(
+                            child: Container(
+                              width: double.infinity,
+                              // TODO: change to background
+                              child: AspectRatio(
+                                aspectRatio: 1 / 1,
+                                child: this._imageFile == null
+                                    ? Placeholder(
+                                        strokeWidth: 1,
+                                        fallbackHeight: 200,
+                                      )
+                                    : Image.file(this._imageFile!),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
                 SizedBox(
                   height: 30,
                 ),
                 Center(
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      if (_title == null || _title!.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Title should not be empty.'),
+                          ),
+                        );
+                        return;
+                      }
+                      var item = Item(
+                        id: '',
+                        uid: user.uid,
+                        title: _title!,
+                        isFullDayEvent: _isFullDayEvent,
+                        formatType: _formatType,
+                        backgroundImage: '',
+                        ts: Timestamp.fromDate(_ts),
+                        ctime: Timestamp.now(),
+                        mtime: Timestamp.now(),
+                      );
+
+                      print(item.toJson());
+                    },
                     child: Text('Add'),
                   ),
                 ),
