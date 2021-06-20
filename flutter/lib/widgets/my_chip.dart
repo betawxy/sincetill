9638fc03290cfb9b3dcccce9c35b8101d94545d3
betawxy <betawxy@gmail.com>
2021-06-19 18:28:30 -0700

    remove unnecessary white bg on chips

--- flutter/lib/widgets/my_chip.dart
+++ flutter/lib/widgets/my_chip.dart
@@ -15,29 +15,23 @@ class MyChip extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: Text(
+        label,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 12 * sizeFactor,
+          color: color,
+        ),
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 5 * sizeFactor,
+        vertical: 3 * sizeFactor,
+      ),
       decoration: BoxDecoration(
-        color: Colors.white,
+        color: color.withOpacity(0.05),
         borderRadius: BorderRadius.circular(3 * sizeFactor),
       ),
-      child: Container(
-        child: Text(
-          label,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 12 * sizeFactor,
-            color: color,
-          ),
-        ),
-        padding: EdgeInsets.symmetric(
-          horizontal: 5 * sizeFactor,
-          vertical: 3 * sizeFactor,
-        ),
-        decoration: BoxDecoration(
-          color: color.withOpacity(0.05),
-          borderRadius: BorderRadius.circular(3 * sizeFactor),
-        ),
-        constraints: BoxConstraints(minWidth: 40 * sizeFactor),
-      ),
+      constraints: BoxConstraints(minWidth: 40 * sizeFactor),
     );
   }
 }
