47683801748c1bb779771175f2dc1061a6465835
betawxy <betawxy@gmail.com>
2021-06-19 18:25:35 -0700

    add speed dial

--- flutter/lib/screens/item_details_screen.dart
+++ flutter/lib/screens/item_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:sincetill/models/item_model.dart';
 import 'package:sincetill/widgets/appbar_title.dart';
 import 'package:sincetill/widgets/item_bg_image_wrapper.dart';
@@ -27,6 +28,30 @@ class ItemDetailsScreen extends StatelessWidget {
             child: ItemDetailsCard(item: item),
           ),
         ),
+      ),
+      floatingActionButton: SpeedDial(
+        icon: Icons.menu,
+        activeIcon: Icons.close,
+        backgroundColor: Theme.of(context).accentColor,
+        foregroundColor: Colors.white,
+        children: [
+          SpeedDialChild(
+            child: Icon(Icons.delete),
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            label: 'Delete',
+            labelStyle: TextStyle(fontSize: 16.0),
+            onTap: () => print('Delete'),
+          ),
+          SpeedDialChild(
+            child: Icon(Icons.edit),
+            backgroundColor: Theme.of(context).primaryColor,
+            foregroundColor: Colors.white,
+            label: 'Edit',
+            labelStyle: TextStyle(fontSize: 16.0),
+            onTap: () => print('Edit'),
+          ),
+        ],
       ),
     );
   }
